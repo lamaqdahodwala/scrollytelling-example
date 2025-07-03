021315c370329fabe8d2fca39644e77769e34415
Lamaq Dahodwala <lamaq091008@gmail.com>
2025-07-03 10:56:56 -0400

    add a sticky div to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,51 @@
 <script lang="ts">
-  import scrollama from "scrollama";
-	import {onMount} from "svelte";
+	import scrollama from 'scrollama';
+	import { onMount } from 'svelte';
 
-  let stepIndex = $state()
-  let directionOfScroll = $state()
-  let progress: number = $state(0)
+	let stepIndex = $state();
+	let directionOfScroll = $state();
+	let progress: number = $state(0);
 
-  onMount(() => {
+	onMount(() => {
+		let scroller = scrollama();
 
-    let scroller = scrollama()
-
-    scroller.setup({
-      step: ".step",
-      progress: true
-    })
-    .onStepEnter(({index, direction}) => {
-        stepIndex = index
-        directionOfScroll = direction
-    })
-    .onStepProgress((response) => {
-        progress = Number(response.progress)
-    }).enable()
-
-  })
-
+		scroller
+			.setup({
+				step: '.step',
+				progress: true
+			})
+			.onStepEnter(({ index, direction }) => {
+				stepIndex = index;
+				directionOfScroll = direction;
+			})
+			.onStepProgress((response) => {
+				progress = Number(response.progress);
+			})
+			.enable();
+	});
 </script>
 
-<div class="fixed right-[5%] h-48 bg-red-200 p-3 border border-1 border-black">
-  <p>Current step: {stepIndex}</p>
-  <p>Direction that step was triggered: {directionOfScroll}</p>
+<div class="fixed right-[5%] h-48 border border-1 border-black bg-red-200 p-3">
+	<p>Current step: {stepIndex}</p>
+	<p>Direction that step was triggered: {directionOfScroll}</p>
 </div>
-<div class="fixed right-[25%] h-48 bg-blue-100 p-3 border border-1 border-black">
-  <p>Your progress through the step</p>
-  <div id="progress-container" class="w-full h-8 bg-gray-300">
-    <div id="progress-bar" style="width: {progress*100}%" class="h-8 flex flex-row justify-center bg-green-800"></div>
-  </div>
-
-  {progress}
+<div class="fixed right-[25%] h-48 border border-1 border-black bg-blue-100 p-3">
+	<p>Your progress through the step</p>
+	<div id="progress-container" class="h-8 w-full bg-gray-300">
+		<div
+			id="progress-bar"
+			style="width: {progress * 100}%"
+			class="flex h-8 flex-row justify-center bg-green-800"
+		></div>
+	</div>
+	{progress}
 </div>
 
-
-<div class="step h-screen font-bold">Hello, I'm step 0</div>
-<div class="step h-screen font-bold">Hello, I'm step 1</div>
-<div class="step h-screen font-bold">Hello, I'm step 2</div>
-<div class="step h-screen font-bold">Hello, I'm step 3</div>
+<div class="">
+	<div class="step h-screen font-bold">Hello, I'm step 0</div>
+<div>
+	<div class="step h-screen font-bold">Hello, I'm step 1</div>
+  <div class="sticky top-1/2">This is a sticky div</div>
+	<div class="step h-screen font-bold">Hello, I'm step 2</div></div>
+	<div class="step h-screen font-bold">Hello, I'm step 3</div>
+</div>
