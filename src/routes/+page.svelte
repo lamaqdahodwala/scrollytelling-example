c84eac930dae72b2bbe14aba4a37f4c6625d9b8e
Lamaq Dahodwala <lamaq091008@gmail.com>
2025-07-08 12:19:10 -0400

    add alternate positioning and sticky

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import scrollama from 'scrollama';
 	import { onMount } from 'svelte';
+	import {
+		Timeline,
+		TimelineConnector,
+		TimelineContent,
+		TimelineDot,
+		TimelineItem,
+		TimelineOppositeContent,
+		TimelineSeparator
+	} from 'svelte-vertical-timeline';
 
 	let stepIndex = $state();
 	let directionOfScroll = $state();
@@ -23,29 +32,28 @@
 			})
 			.enable();
 	});
+
+	let events = ['2021', '2022', '2023', '2024'];
 </script>
 
-<div class="fixed right-[5%] h-48 border border-1 border-black bg-red-200 p-3">
-	<p>Current step: {stepIndex}</p>
-	<p>Direction that step was triggered: {directionOfScroll}</p>
-</div>
-<div class="fixed right-[25%] h-48 border border-1 border-black bg-blue-100 p-3">
-	<p>Your progress through the step</p>
-	<div id="progress-container" class="h-8 w-full bg-gray-300">
-		<div
-			id="progress-bar"
-			style="width: {progress * 100}%"
-			class="flex h-8 flex-row justify-center bg-green-800"
-		></div>
-	</div>
-	{progress}
-</div>
-
-<div class="">
-	<div class="step h-screen font-bold">Hello, I'm step 0</div>
-<div>
-	<div class="step h-screen font-bold">Hello, I'm step 1</div>
-  <div class="sticky top-1/2">This is a sticky div</div>
-	<div class="step h-screen font-bold">Hello, I'm step 2</div></div>
-	<div class="step h-screen font-bold">Hello, I'm step 3</div>
-</div>
+<div class="fixed"></div>
+<Timeline position="alternate">
+	{#each events as i}
+		<TimelineItem>
+			<TimelineSeparator>
+				<TimelineDot />
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<div class="step h-screen grow">
+					<p class="sticky top-1/2">{i}</p>
+				</div>
+			</TimelineContent>
+			<TimelineOppositeContent slot="opposite-content">
+				<div>
+          <p class="h-64">{progress}</p>
+        </div>
+			</TimelineOppositeContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
